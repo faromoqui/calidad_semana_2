39610527e926408942a68b838fb476d07a54bf77
Fabian Montenegro <faromoqui@gmail.com>
2025-01-09 19:41:37 -0500

    mensaje 2

--- test/test3.py
+++ test/test3.py
@@ -1,14 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
-import time
 import unittest
+import time
 
+# Configurar opciones para el navegador Chrome
+options = Options()
+options.add_argument('--headless') # Ejecuta Chrome en modo headless
+options.add_argument('--no-sandbox') # Recomendado en entornos de CI como GitHub Actions
+options.add_argument('--disable-dev-shm-usage') # Ayuda a evitar algunos errores en contenedores
 
 class eis_test(unittest.TestCase):
 
 	def setUp(self):
-		self.browser = webdriver.Chrome()
+		self.browser = webdriver.Chrome(options=options)
 
 	def test_load(self):
 		browser = self.browser
